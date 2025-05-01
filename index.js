09cf436d7b3dfa318972b38a46254e11429ad417
Rostyslav Nahornyi <rostyslav.nahornyi.official@gmail.com>
2025-05-02 00:21:02 +0200

    1

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const main = async () => {
 
   try {
     const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+    const page = (await browser.newPage()).setDefaultNavigationTimeout(60000);
 
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
     );
-    await page.goto(URL, { waitUntil: "networkidle2" });
+    await page.goto(URL, { waitUntil: "networkidle2", timeout: 60000 });
 
     const html = await page.content();
     const $ = cheerio.load(html);
