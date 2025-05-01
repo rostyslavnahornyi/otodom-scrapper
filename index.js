01be7c49523ebfe719b3e99d46c14b346dd1e5b6
Rostyslav Nahornyi <rostyslav.nahornyi.official@gmail.com>
2025-05-01 23:52:45 +0200

    sonya

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ import puppeteer from "puppeteer";
 
 const DOMAIN = "https://www.otodom.pl";
 const URL =
-  "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie/malopolskie/krakow/krakow/krakow?limit=36&extras=%5BIS_PRIVATE_OWNER%5D&by=LATEST&direction=DESC&viewType=listing";
-const botToken = "6099185644:AAG9MvRPtxP8oHsw2doiiEQFNNTVcnwE9hI";
+  "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie,2-pokoje/wiele-lokalizacji?limit=36&priceMax=3500&daysSinceCreated=7&locations=%5Bmalopolskie%2Fkrakow%2Fkrakow%2Fkrakow%2Fczyzyny%2Cmalopolskie%2Fkrakow%2Fkrakow%2Fkrakow%2Fmistrzejowice%2Cmalopolskie%2Fkrakow%2Fkrakow%2Fkrakow%2Fbienczyce%2Cmalopolskie%2Fkrakow%2Fkrakow%2Fkrakow%2Fnowa-huta%2Fnowa-huta%5D&by=LATEST&direction=DESC&viewType=listing";
+const botToken = "7631606729:AAEnMP8K1fdX8QCe2c2RH55imYtOaqBHHoI";
 
 const bot = new Telegraf(botToken);
 
 const main = async () => {
+  console.log("MAIN START");
   const dbOld = JSON.parse(fs.readFileSync("./apartaments.json"));
   const allUrls = [...dbOld.oldUrls, ...dbOld.newUrls];
-  const db = { newUrls: [], oldUrls: allUrls };
+  const db = { newUrls: [], oldUrls: allUrls, users: dbOld.users };
 
   try {
     const browser = await puppeteer.launch({ headless: true });
@@ -37,8 +38,11 @@ const main = async () => {
     organicListings.each((_, el) => {
       $(el)
         .find('[data-cy="listing-item-link"]')
-        .each((_, link) => {
-          const href = $(link).attr("href");
+        .each((i, link) => {
+          const href = !$(link).attr("href").includes("/hpr/")
+            ? $(link).attr("href")
+            : undefined;
+          console.log(i, { href });
           if (href) {
             const fullUrl = href.startsWith("http") ? href : DOMAIN + href;
             if (!allUrls.includes(fullUrl)) {
@@ -58,15 +62,23 @@ const main = async () => {
   db.length = db.newUrls.length + db.oldUrls.length;
 
   if (db.newUrls.length !== 0) {
-    await bot.telegram.sendMessage(
-      409287724,
-      db.newUrls.join("\n\n") +
-        `\n\nall: ${db.length}\nnewUrls: ${db.newUrls.length}\noldUrls: ${db.oldUrls.length}`
-    );
+    dbOld.users.forEach(async (id) => {
+      await bot.telegram.sendMessage(
+        id,
+        db.newUrls.join("\n\n") +
+          `\n\nall: ${db.length}\nnewUrls: ${db.newUrls.length}\noldUrls: ${db.oldUrls.length}`
+      );
+    });
   }
 
   fs.writeFileSync("./apartaments.json", JSON.stringify(db, null, 2));
 };
 
-setInterval(main, 3000);
-t;
+bot.start((ctx) => {
+  const db = JSON.parse(fs.readFileSync("./apartaments.json"));
+
+  db.users = [...db.users, ctx.chat.id];
+  fs.writeFileSync("./apartaments.json", JSON.stringify(db, null, 2));
+});
+
+bot.launch(() => setInterval(main, 7500));
